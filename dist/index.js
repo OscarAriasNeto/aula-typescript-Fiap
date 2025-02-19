956b7030f4740b626b7ccdba7acca8e9684287ea
Oscar Arias Neto <kazinhoarias9@gmail.com>
2025-02-19 20:50:58 -0300

    Exercicios inicias de Typescript, (Bem vindo, soma e carro)

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,21 @@
 let mensagem = "Olá, TypeScript!";
 console.log(mensagem);
+let valores = [60, 50, 40];
+let somaValores = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
+function mensagemPersonalizada(nome) {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+console.log(mensagemPersonalizada("Oscar"));
+class Carro {
+    constructor(marca, modelo, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    detalhes() {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
+}
+const carro1 = new Carro("Audi", "R8", 2024);
+console.log(carro1.detalhes());
